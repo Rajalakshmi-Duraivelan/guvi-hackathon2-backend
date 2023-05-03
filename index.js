99a12fe634be9674431d92f95479c12b592e3c7b
Rajalakshmi Duraivelan <121893509+Rajalakshmi-Duraivelan@users.noreply.github.com>
2023-05-03 17:13:51 +0530

    Updated url

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const mongoclient = new mongodb.MongoClient(URL)
 app.use(express.json());
 
 app.use(cors({
-    origin:"http://localhost:3000"
-    //origin:"https://equipment-rental-portal.netlify.app"
+    //origin:"http://localhost:3000"
+    origin:"https://equipment-rental-portall.netlify.app"
 }))
 
 app.get("/", function (req, res) {
